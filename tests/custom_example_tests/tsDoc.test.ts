baf943adb79c6ef072072e2a46ff8400992ccd67
asvynarov <--global>
2022-05-25 13:14:18 +0300

    added demo tests to tsDoc.test

--- tests/custom_example_tests/tsDoc.test.ts
+++ tests/custom_example_tests/tsDoc.test.ts
@@ -7,3 +7,17 @@ t.test("Typescript documentation's page redirects user to npm page", async () =>
     await t.click('[href="https://www.npmjs.com/package/typescript"]');
     expect(await t.text('[title="typescript"]')).to.be.equal('typescript');
 });
+
+t.test("Typescript documentation's page search tool redirects user to valid page", async() => {
+    await t.go('https://www.typescriptlang.org/');
+    await t.type('[id="search-box-top"]', 'generics');
+    await t.click('[class="algolia-docsearch-suggestion--highlight"]');
+    expect(await t.text('[id="handbook-content"] h2')).to.equal('Generics');
+});
+
+t.test("Typescript documentation's page allow user to run playground tool", async () => {
+    await t.go('https://www.typescriptlang.org/');
+    await t.click('[href="/play"]');
+    await t.click('[id="run-button"]');
+    expect(await t.text('[id="log"]')).contains('"Hello World"');
+});
